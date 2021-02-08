208044ab95fc76086a44323eefcb08fd87b7eacf
jahusby <32870250+jahusby@users.noreply.github.com>
2021-02-08 12:58:06 +0100

    first commit
    added netflix frontend with data from  tmdb api. Hosting with firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDEq-j_hAOEbjwvcH7KGZKNGGcQekIjFbs",
+  authDomain: "netflix-build-295fe.firebaseapp.com",
+  projectId: "netflix-build-295fe",
+  storageBucket: "netflix-build-295fe.appspot.com",
+  messagingSenderId: "299989733122",
+  appId: "1:299989733122:web:03e97d35bda932dfbf3c1b",
+};
