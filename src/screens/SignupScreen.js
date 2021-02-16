7424e82c547a93fee6da520318d896a8ea0a414c
jahusby <32870250+jahusby@users.noreply.github.com>
2021-02-16 11:31:23 +0100

    LoginScreen
    First commit login/signup frontend

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SignupScreen.css";
+
+function SignupScreen() {
+  return (
+    <div className="signupScreen">
+      <h1>sign in screen</h1>
+    </div>
+  );
+}
+
+export default SignupScreen;
